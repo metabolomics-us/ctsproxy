94f142b8251eef526ce987b19a5049f27e6515d7
Matthew Mueller <matthewmueller@dhcp44-gc5.genomecenter.ucdavis.edu>
2017-03-27 11:44:31 -0700

    fixed batch conversion, improved error handling

--- src/main/resources/static/js/service.js
+++ src/main/resources/static/js/service.js
@@ -46,10 +46,17 @@ app.controller('MainCtrl', ['$scope','$http', function($scope,$http) {
     $scope.batchConversion = function(searchTerm,from,to) {
         var terms = searchTerm.split('\n');
         terms.forEach(function (term,i) {
+            $scope.response.data[i] = {'searchTerm':term,'result':['Loading...']};
             $http.get('/rest/convert/'+from+'/'+to+'/'+term)
                 .then(function(response) {
                     console.log(response);
                     $scope.response.data[i] = response.data[0];
+                    if (response.data[0].result.length == 0) {
+                        $scope.response.data[i].result = ['None found'];
+                    }
+                }, function(response) {
+                    console.log(response);
+                    $scope.response.data[i].result = ['Error'];
                 });
         });
     };
