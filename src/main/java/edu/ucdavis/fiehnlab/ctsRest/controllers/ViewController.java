bc238fd70c3cced5cb8cf3b4d2963b6b5fdff9e6
Matthew Mueller <matthewmueller@minimac.local>
2018-03-07 11:41:43 -0800

    fixed routing

---
+++ src/main/java/edu/ucdavis/fiehnlab/ctsRest/controllers/ViewController.java
@@ -0,0 +1,20 @@
+package edu.ucdavis.fiehnlab.ctsRest.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ViewController {
+    /*
+    * This is necessary because the Angular app is using HTML5 Mode
+    * Routing is handled on the client side; the server simply needs to serve index.html
+    *
+    * */
+    @RequestMapping({
+            "/batch",
+            "/services"
+    })
+    public String index() {
+        return "forward:/index.html";
+    }
+}
