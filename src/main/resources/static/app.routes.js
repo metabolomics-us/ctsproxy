b3ae5dadc2d91dae758e6c34d814902c630e2ffc
Matthew Mueller <matthewmueller@minimac.local>
2018-03-08 14:26:54 -0800

    structural refactoring

--- src/main/resources/static/app.routes.js
+++ src/main/resources/static/app.routes.js
@@ -13,10 +13,4 @@ angular.module('cts')
 
         $locationProvider.html5Mode(true);
         $locationProvider.hashPrefix("");
-    }).run(['$anchorScroll', function($anchorScroll) {
-        $anchorScroll.yOffset = 60;
-    }]).run(['$rootScope', function($rootScope) {
-        $rootScope.APP_NAME = 'The Chemical Translation Service';
-        $rootScope.APP_NAME_ABBR = 'CTS';
-        $rootScope.APP_VERSION = 'v1.0';
-    }]);
+    });
