fb8082ed27e9969c2ccd282f98461d6ae84624e5
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2019-01-08 16:35:15 -0500

    added basic google analytics to web app

--- web/src/main/resources/static/shared/config/config.js
+++ web/src/main/resources/static/shared/config/config.js
@@ -2,12 +2,16 @@
     'use strict';
 
     angular.module('cts')
-          .run(['$anchorScroll', '$rootScope', function ($anchorScroll, $rootScope) {
-              $anchorScroll.yOffset = 60;
+        .run(['$anchorScroll', '$rootScope', function ($anchorScroll, $rootScope) {
+          $anchorScroll.yOffset = 60;
 
-              $rootScope.APP_NAME = 'The Chemical Translation Service';
-              $rootScope.APP_NAME_ABBR = 'CTS';
-              $rootScope.APP_VERSION = 'v1.0';
-          }]);
+          $rootScope.APP_NAME = 'The Chemical Translation Service';
+          $rootScope.APP_NAME_ABBR = 'CTS';
+          $rootScope.APP_VERSION = 'v1.0';
+        }]);
 
+    angular.module('cts')
+        .config(['AnalyticsProvider', function (AnalyticsProvider) {
+            AnalyticsProvider.setAccount('UA-39617172-1');
+        }]).run(['Analytics', function(Analytics) {}])
 })();
