e57d1c7fd107c98d45229f5b0b6125f6f63f6109
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2019-01-11 10:38:21 -0600

    remove unneeded test configuration

--- web/src/test/scala/edu/ucdavis/fiehnlab/ctsrest/web/CtsProxyTestsConfig.scala
+++
@@ -1,9 +0,0 @@
-package edu.ucdavis.fiehnlab.ctsrest.web
-
-import org.springframework.cache.annotation.EnableCaching
-
-/**
- * Created by diego on 2/21/2017.
- */
-@EnableCaching
-class CtsProxyTestsConfig
